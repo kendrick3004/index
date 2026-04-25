c901efe9614865b84b50252d36ceebe1dcacf1cc
kendrick3004 <101604411+kendrick3004@users.noreply.github.com>
2026-04-25 13:16:30 -0300

    Auto update

--- setup.sh
+++ setup.sh
@@ -16,16 +16,16 @@ echo "[2/3] Instalando pacotes base (nano, curl, wget, openssh-server, net-tools
 sudo apt install -y nano curl wget openssh-server net-tools git python3 python3-pip python3-venv 
 sudo apt install python3 python3-pip python3-venv -y
 
-echo "[3/3] Ativando o Cloudflared Tunnel"
-sudo pkill cloudflared
-sudo cloudflared service uninstall
-sudo rm -f /etc/init.d/cloudflared
-sudo rm -rf /etc/cloudflared
-sudo apt remove --purge cloudflared -y
-sudo apt autoremove -y
-rm -rf ~/.cloudflared
-sudo apt-get update && sudo apt-get install cloudflared -y
-sudo cloudflared service install eyJhIjoiOWNjZGQzMjk0NDllMzJhZWU4YzRkYWRkMDZjOGM0NzciLCJ0IjoiOTU5Njg0NDUtYzBhNS00M2RhLTk1ZDctYzYxY2I0YzU3NzZjIiwicyI6Ik5HWmlPRFF3WVRJdFpHUTNaaTAwWkdOakxUa3dZV1F0WkdRME5qbGhZV1E0TkRreU5XSXpZVGsyTW1FdE1XWmxPUzAwTjJGa0xUbG1Oek10TkdWbU9EQTVOV0ppWXpkbCJ9
+#echo "[3/3] Ativando o Cloudflared Tunnel"
+#sudo pkill cloudflared
+#sudo cloudflared service uninstall
+#sudo rm -f /etc/init.d/cloudflared
+#sudo rm -rf /etc/cloudflared
+#sudo apt remove --purge cloudflared -y
+#sudo apt autoremove -y
+#rm -rf ~/.cloudflared
+#sudo apt-get update && sudo apt-get install cloudflared -y
+#sudo cloudflared service install eyJhIjoiOWNjZGQzMjk0NDllMzJhZWU4YzRkYWRkMDZjOGM0NzciLCJ0IjoiOTU5Njg0NDUtYzBhNS00M2RhLTk1ZDctYzYxY2I0YzU3NzZjIiwicyI6Ik5HWmlPRFF3WVRJdFpHUTNaaTAwWkdOakxUa3dZV1F0WkdRME5qbGhZV1E0TkRreU5XSXpZVGsyTW1FdE1XWmxPUzAwTjJGa0xUbG1Oek10TkdWbU9EQTVOV0ppWXpkbCJ9
 
 echo "================================================"
 echo "✅ Setup concluído com sucesso!"
