39b23d0234cd11f60bfbbff179b83f8819be06a9
kendrick3004 <101604411+kendrick3004@users.noreply.github.com>
2026-04-28 13:53:36 -0300

    Rebuild completo dos arquivos

--- setup.sh
+++ setup.sh
@@ -16,7 +16,16 @@ echo "[2/3] Instalando pacotes base (nano, curl, wget, openssh-server, net-tools
 sudo apt install -y nano curl wget openssh-server net-tools git python3 python3-pip python3-venv 
 
 echo "[3/3] Ativando o Cloudflared Tunnel"
+# Add cloudf
 # Add cloudflare gpg key
+sudo mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-public-v2.gpg | sudo tee /usr/share/keyrings/cloudflare-public-v2.gpg >/dev/null
+
+# Add this repo to your apt repositories
+echo 'deb [signed-by=/usr/share/keyrings/cloudflare-public-v2.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+
+# install cloudflared
+sudo apt-get update && sudo apt-get install cloudflared
 sudo mkdir -p --mode=0755 /usr/share/keyrings
 curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
 
@@ -26,7 +35,7 @@ echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudf
 # install cloudflared
 sudo apt-get update && sudo apt-get install cloudflared -y
 # O comando abaixo pode falhar se o serviço já estiver instalado ou se o token for inválido/expirado
-sudo cloudflared service install eyJhIjoiOWNjZGQzMjk0NDllMzJhZWU4YzRkYWRkMDZjOGM0NzciLCJ0IjoiOTU5Njg0NDUtYzBhNS00M2RhLTk1ZDctYzYxY2I0YzU3NzZjIiwicyI6Ik5HWmlPRFF3WVRJdFpHUTNaaTAwWkdOakxUa3dZV1F0WkdRME5qbGhZV1E0TkRreU5XSXpZVGsyTW1FdE1XWmxPUzAwTjJGa0xUbG1Oek10TkdWbU9EQTVOV0ppWXpkbCJ9 || true
+sudo cloudflared service install sudo cloudflared service install eyJhIjoiOWNjZGQzMjk0NDllMzJhZWU4YzRkYWRkMDZjOGM0NzciLCJ0IjoiNWJjMjkyOTEtMzdmYi00ZTAwLWJjN2EtZjlmYmYzNzJkMDM3IiwicyI6Ik1tVTVPR0kyTlRndFkyRmtNUzAwTm1RNExXRmpZamd0TjJObE1UTTRNVEkwTXpRMyJ9 || true
 
 echo "================================================"
 echo "✅ Setup concluído com sucesso!"
